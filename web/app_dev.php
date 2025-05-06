8d1a8d48f9085b2cff45304ab987508cb116d6df
Tomi Belan <tomi.belan@gmail.com>
2025-05-06 14:47:53 +0200

    symfony-standard 2.8 (partial), sensio/distribution-bundle 5.0
    mostly based on: https://github.com/symfony/symfony-standard/compare/2.3...2.8
    
    Sensio Distribution Bundle must be updated because our old version (2.3) is not
    compatible with Composer 2. our other dependencies were left alone for now.
    
    for the record, a needlessly detailed explanation of the changes:
    
    - composer.json
      - manually changed, inspired by the symfony-standard diff (partial)
    
    - composer.lock
    - app/SymfonyRequirements.php
    - app/check.php
    - web/config.php
      - auto-generated by running "composer update"
    
    - .gitignore
    - app/config/routing_dev.yml
    - web/app_dev.php
      - manually applied some equivalent of the symfony-standard diff
    
    - app/.htaccess
    - app/AppCache.php
    - app/console
    - src/.htaccess
    - web/robots.txt
    - web/app.php
      - was perfectly equal to symfony-standard 2.3 or some older version
      - replaced with 2.8
    
    - app/autoload.php
      - was almost equal to symfony-standard 2.1 except comments
      - replaced with 2.8
    
    - app/phpunit.xml.dist
      - was similar to symfony-standard 2.0-2.2, but with custom <logging>
      - manually merged with symfony-standard 2.8
    
    - web/.htaccess
      - was almost equal to symfony-standard 2.1 except cosign/valid
      - replaced with 2.8
      - but we never use this .htaccess anyway
    
    - app/config/config_dev.yml
    - app/config/config_prod.yml
      - no change, but may need revisiting if we update Monolog
    
    - LICENSE
    - README.md
    - app/AppKernel.php
    - app/Resources/views/default/index.html.twig
    - app/config/config.yml
    - app/config/parameters.yml.dist
    - app/config/routing.yml
    - app/config/security.yml
    - app/config/services.yml
    - src/AppBundle/Controller/DefaultController.php
    - web/apple-touch-icon.png
      - no change - symfony-standard diff ignored

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,12 +1,10 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+require __DIR__.'/../app/autoload.php';
 Debug::enable();
-
-require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
